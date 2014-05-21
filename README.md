c47b79e5bcaf65fd403465aa6f07ef207d0201e1
simonp <simon.plasger@web.de>
2014-05-21 11:59:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+Roundcube Plugin Spam Vpopmaild
+===============================
+
+Setup
+-----
+
+0. Make sure that your installation of vpopmaild. If you need to know
+   how to setup the daemon, see `/doc/README.vpopmaild` in the source folder
+   of vpopmail.
+
+1. Copy the files `spam_vpopmaild.php`, `config.inc.php.dist` and the 
+   folder `localization` to a folder named `spam_vpopmaild` into the 
+   directory `plugins` of your Roundcube installation.
+   
+2. Rename `config.inc.php.dist` to `config.inc.php`.
+
+3. Adjust the values in `config.inc.php` to the values that match to
+   your setup.
+   
+Usage
+-----
+
+Access the plugin options via Settings -> Spam protection.
+
+Requirements
+------------
+
+Tested with Roundcube 1.0.1 and PHP 5.4.4 but should work with older versions.
+
+License
+-------
+
+Roundcube Plugin Spam Vpopmaild let's you adjust the spam preferences via vpopmaild.
+
+Copyright (C) 2014 Simon Plasger
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+See COPYING for details.
---
+++ spam_vpopmaild.php
@@ -0,0 +1,217 @@
+<?php
+/**
+ * Spam vpopmaild
+ *
+ * Roundcube plugin to toggle the spam settings for a user via vpopmaild
+ *
+ * @version 1.0
+ * @author Simon Plasger
+ * @url 
+ * @license GPLv3+
+ */
+class spam_vpopmaild extends rcube_plugin
+{
+    public $task    = 'settings';
+    public $noframe = true;
+    public $noajax  = true;
+
+    function init()
+    {
+        $rcmail = rcmail::get_instance();
+        $this->load_config();
+        
+        // Tab in settings
+        $this->add_hook('settings_actions', array($this, 'settings_tab'));
+	
+	    $this->register_action('plugin.spam_vpopmaild', array($this, 'spam_vpopmaild_init'));
+        $this->register_action('plugin.spam_vpopmaild-save', array($this, 'spam_vpopmaild_save'));
+    }
+    
+    function settings_tab($args)
+    {
+        $this->add_texts('localization/');
+        $args['actions'][] = array('action' => 'plugin.spam_vpopmaild', 'type' => 'link', 'class' => 'spam_vpopmaild', 'label' => 'spam_vpopmaild.spam_vpopmaild', 'title' => 'spam_vpopmaild.spam_vpopmaild');
+        return $args;
+    }
+    
+    function spam_vpopmaild_init()
+    {
+        $this->add_texts('localization/');
+        $this->register_handler('plugin.body', array($this, 'spam_vpopmaild_form'));
+        $rcmail = rcmail::get_instance();
+        $rcmail->output->set_pagetitle($this->gettext('spam_on_off'));
+        $rcmail->output->send('plugin');
+    }
+    
+    function spam_vpopmaild_save()
+    {
+        $rcmail = rcmail::get_instance();
+        $this->add_texts('localization/');
+        
+        $this->register_handler('plugin.body', array($this, 'spam_vpopmaild_form'));
+        $rcmail->output->set_pagetitle($this->gettext('spam_on_off'));
+        if(isset($_POST['spamassassin']))
+        {
+            $no_spamassassin = 0;
+        }
+        else
+        {
+            $no_spamassassin = 1;
+        }
+        if(isset($_POST['delete_spam']))
+        {
+            $delete_spam = 1;
+        }
+        else
+        {
+            $delete_spam = 0;
+        }
+        $result = $this->set_state($rcmail->decrypt($_SESSION['password']), $no_spamassassin, $delete_spam);
+        if($result)
+            $rcmail->output->command('display_message', $this->gettext('success'), 'confirmation');
+        else
+            $rcmail->output->command('display_message', $this->gettext('save_error'), 'error');
+            
+        $rcmail->overwrite_action('plugin.spam_vpopmaild');
+        $rcmail->output->send('plugin');
+    }
+    
+    function load_state($pass)
+    {
+        $rcmail = rcmail::get_instance();
+        
+        $vpopmaild = new Net_Socket();
+        
+        // Connect to server
+        if (PEAR::isError($vpopmaild->connect($rcmail->config->get('spam_vpopmaild_host'),
+            $rcmail->config->get('spam_vpopmaild_port'), null))) {
+            return false;
+        }
+        
+        $vpopmaild->setTimeout($rcmail->config->get('password_vpopmaild_timeout'),0);
+        
+        $result = $vpopmaild->readLine();
+        if(!preg_match('/^\+OK/', $result)) {
+            $vpopmaild->disconnect();
+            return false;
+        }
+        
+        // login
+        $vpopmaild->writeLine("login ". $_SESSION['username'] . " " . $pass);
+        
+        $result = $vpopmaild->readLine();
+
+        if(!preg_match('/^\+OK+/', $result) ) {
+            $vpopmaild->writeLine("quit");
+            $vpopmaild->disconnect();
+            return false;
+        }
+        $return = array();
+        
+        // fetch results
+        do
+        {
+            $result = $vpopmaild->readLine();
+            if(preg_match('/^no_spamassassin 0/', $result))
+            {
+                $return['no_spamassassin'] = 0;
+            }
+            else if(preg_match('/^no_spamassassin 1/', $result))
+            {
+                $return['no_spamassassin'] = 1;
+            }
+            else if(preg_match('/^delete_spam 0/', $result))
+            {
+                $return['delete_spam'] = 0;
+            }
+            else if(preg_match('/^delete_spam 1/', $result))
+            {
+                $return['delete_spam'] = 1;
+            }
+        }
+        while(!preg_match('/^\./', $result));
+        $vpopmaild->writeLine("quit");
+        $vpopmaild->disconnect();
+        return $return;
+    }
+    
+    function set_state($pass, $no_spamassassin, $delete_spam)
+    {
+        $rcmail = rcmail::get_instance();
+        
+        $vpopmaild = new Net_Socket();
+        
+        if (PEAR::isError($vpopmaild->connect($rcmail->config->get('spam_vpopmaild_host'),
+            $rcmail->config->get('spam_vpopmaild_port'), null))) {
+            return false;
+        }
+        
+        $vpopmaild->setTimeout($rcmail->config->get('password_vpopmaild_timeout'),0);
+        
+        $result = $vpopmaild->readLine();
+        if(!preg_match('/^\+OK/', $result)) {
+            $vpopmaild->disconnect();
+            return false;
+        }
+        
+        // slogin = silent login, don't show all user infos after login
+        $vpopmaild->writeLine("slogin ". $_SESSION['username'] . " " . $pass);
+        
+        $result = $vpopmaild->readLine();
+
+        if(!preg_match('/^\+OK+/', $result) ) {
+            $vpopmaild->writeLine("quit");
+            $vpopmaild->disconnect();
+            return false;
+        }
+        
+        $vpopmaild->writeLine("mod_user ". $_SESSION['username']);
+        $vpopmaild->writeLine("no_spamassassin " . $no_spamassassin);
+        $vpopmaild->writeLine("delete_spam " . $delete_spam);
+        $vpopmaild->writeLine(".");
+        $vpopmaild->writeLine("quit");
+        $vpopmaild->disconnect();
+        return true;
+    }
+       
+    function spam_vpopmaild_form()
+    {
+        $rcmail = rcmail::get_instance();
+        
+        $state = $this->load_state($rcmail->decrypt($_SESSION['password']));
+        
+        if(!is_array($state))
+        {
+            $state = array('no_spamassassin' => 1, 'delete_spam' => 0);
+            $rcmail->output->command('display_message', $this->gettext('load_error'), 'error');
+        }
+            
+        $rcmail->output->set_env('product_name', $rcmail->config->get('product_name'));
+        
+        $table = new html_table(array('cols' => 2));
+        $field_id = 'spam_vpopmaild_detect';
+        $input_togglespam = new html_checkbox(array('name' => 'spamassassin', 'id' => $field_id, 'value' => 'spamassassin'));
+        $table->add('title', html::label($field_id, $this->gettext('detection')));
+        $table->add(null, $input_togglespam->show($state['no_spamassassin'] == 1 ? '' : 'spamassassin'));
+        $field_id = 'spam_vpopmaild_delete';
+        $input_togglespam = new html_checkbox(array('name' => 'delete_spam', 'id' => $field_id, 'value' => 'delete_spam'));
+        $table->add('title', html::label($field_id, $this->gettext('delete')));
+        $table->add(null, $input_togglespam->show($state['delete_spam'] == 0 ? '' : 'delete_spam'));
+        $submit = new html_inputfield(array('type' => 'submit', 'id' => 'spam_vpopmaild_save', 'class' => 'button mainaction', 'value' => $this->gettext('save')));
+        
+        $out = html::div(array('class' => 'box'), 
+               html::div(array('id' => 'prefs-title', 'class' => 'boxtitle'), $this->gettext('spam_on_off')) .
+               html::div(array('class' => 'boxcontent'), $table->show() .
+               html::p('formbuttons', $submit->show()
+               )));
+                
+       $rcmail->output->add_gui_object('spamform', 'spam_vpopmaild-form');
+       
+       return $rcmail->output->form_tag(array(
+           'id' => 'spam_vpopmaild-form',
+           'name' => 'spam_vpopmaild-form',
+           'method' => 'post',
+           'action' => './?_task=settings&_action=plugin.spam_vpopmaild-save',
+       ), $out);
+    } 
+}
